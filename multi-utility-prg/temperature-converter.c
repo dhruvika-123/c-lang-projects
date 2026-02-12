d14ae15d01c4f18c66a7db87f97d68b41b32da71
dhruvika-123 <dhruvika2500@gmail.com>
2026-02-12 10:45:34 +0530

    add project

---
+++ multi-utility-prg/temperature-converter.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+float C;
+float F;
+
+printf("Enter temperature in °C");
+scanf("%f", &C);
+printf("Temperature in °F = %f", (9.0/5.0 *C) + 32);
+return 0;
+
+
+
+
+
+
+    
+}
